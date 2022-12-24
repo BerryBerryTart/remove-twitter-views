14021069bbd424a67c787c063e1788491be12459
Zuberi Reid <windbird44@gmail.com>
2022-12-24 02:50:00 -0500

    menu item fix

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         I HATE YOU VIEWS
 // @namespace    http://tampermonkey.net/
-// @version      1.05
+// @version      1.06
 // @description  go away you little bitch
 // @author       BEWWY
 // @match        https://twitter.com/*
@@ -14,18 +14,18 @@ let enableOnScrollCall = true;
 const GENERAL_THROTTLE_LIMIT = 400;
 const THROTTLE_LIMIT = 100;
 
-(function() {
+(function () {
     setInterval(removeViewsFromDOM, GENERAL_THROTTLE_LIMIT);
 })();
 
-window.addEventListener('scroll', function(e){
+window.addEventListener('scroll', function (e) {
     if (!enableOnScrollCall) return;
     enableOnScrollCall = false;
     removeViewsFromDOM();
-    setTimeout(function(){ enableOnScrollCall = true}, THROTTLE_LIMIT);
+    setTimeout(function () { enableOnScrollCall = true }, THROTTLE_LIMIT);
 });
 
-function removeViewsFromDOM(){
+function removeViewsFromDOM() {
     let removalList = document.querySelectorAll("a[href$='analytics']");
     if (removalList.length === 0) return;
     let cleanupList = [];
@@ -35,14 +35,16 @@ function removeViewsFromDOM(){
             let a = e.parentNode;
             let b = a.parentNode; //why twitter
             cleanupList.push(b);
-            a.remove();
+            if (e.role !== "menuitem") {
+                a.remove();
+            }
         }
     );
     //bodge to clean up the empty div left over
-    if (cleanupList.length > 0){
+    if (cleanupList.length > 0) {
         cleanupList.forEach(
             e => {
-                if(!e.childNodes.length){
+                if (!e.childNodes.length) {
                     e.remove();
                 }
             }
